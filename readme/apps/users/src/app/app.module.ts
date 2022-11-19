39b598f6703b18e004a2aac7fae6e26ecf757ac5
Nikita <nikita.krymov@bk.ru>
2022-11-19 18:01:23 +0300

    модули для пользователя и авторизации

--- readme/apps/users/src/app/app.module.ts
+++ readme/apps/users/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlogUserModule } from './blog-user/blog-user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [BlogUserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
